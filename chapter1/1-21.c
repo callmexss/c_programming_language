218ff262d4fa792e7ca0f06d6df6d4fc79343b44
root <root@localhost.localdomain>
2018-01-05 10:33:08 -0500

    1-20&&1-21 need to understand.

--- chapter1/1-21.c
+++ chapter1/1-21.c
@@ -4,10 +4,37 @@
 
 int main(int argc, char *argv[])
 {
-  int c, start, end;
+  int c, nb, nt, pos;
 
-  while ((c = getchar()) != EOF)
+  nb = 0;
+  nt = 0;
+  for (pos = 1; (c = getchar()) != EOF; pos++)
   {
+    if (c == ' ')
+    {
+      if (pos % TABINC != 0)
+        nb++;
+      else
+      {
+        nb = 0;
+        nt++;
+      }
+    } // end of if
+    else
+    {
+      for (; nt > 0; nt--)
+        putchar('\t');
+      if (c == '\t')
+        nb == 0;
+      else
+        for (; nb > 0; nb--)
+          putchar(' ');
+      putchar(c);
+      if (c == '\n')
+        pos = 0;
+      else if (c == '\t')
+        pos = pos + (TABINC - (pos - 1) % TABINC) - 1;
+    }
   }
   
 
